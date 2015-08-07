d17300759f4092759b2e1713538c3a4a66b2ecc4
TechieDev <techiedev1987@gmail.com>
2015-08-07 16:34:33 +0530

    test implemented for course model.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Course do
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
+  it "saves itself" do
+  	course = Course.new(:title => "course1", :description => "this is test for course1")
+  	course.save
+  	expect(Course.first).to eq(course)
+  end
 end
