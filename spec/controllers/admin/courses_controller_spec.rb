53aaef829feac76dc57d5f7654c0dd0100129de1
TechieDev <techiedev1987@gmail.com>
2015-09-09 12:51:09 +0530

    Course controller rspec tests completed.

--- spec/controllers/admin/courses_controller_spec.rb
+++ spec/controllers/admin/courses_controller_spec.rb
@@ -84,7 +84,95 @@ describe Admin::CoursesController do
     			post :create, course: {title: "workshop on IT Recruitment", description: "In this workshop, one will learn how they can do IT Recruitment"}
     			expect(response).to redirect_to admin_courses_path
     		end
+            it "sets the flash success message" do
+                post :create, course: {title: "workshop on IT Recruitment", description: "In this workshop, one will learn how they can do IT Recruitment"}
+                expect(flash[:success]).to be_present 
+           end
+        end
+        context "with invalid attributes" do
+            login_admin
+            it "doesn't create course if title is not input" do
+                post :create, course: {description: "In this workshop, one will learn how they can do IT Recruitment"}
+                expect(Course.count).to eq(0)
+            end
+            it "renders the new course page" do
+                post :create, course: {description: "In this workshop, one will learn how they can do IT Recruitment"}
+                expect(response).to render_template :new 
+            end
+            it "flashes the error message" do
+               post :create, course: { description: "what you should know about a safe workplace" }
+               expect(flash[:alert]).to be_present 
+            end
+        end
+    end
 
+    describe "PUT #update" do
+       let(:course) { FactoryGirl.create(:course) } 
+       it_behaves_like "sign in mandatory" do
+            let(:action) { post :update, id: course.id }
+        end
+       it_behaves_like "admin mandatory" do
+            let(:action) { post :update, id: course.id }
+       end
+       context "with valid attributes" do
+          login_admin
+          it "updates an existing course" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: course.description } 
+            course.reload
+            expect(Course.find(course.id).title).to eq(course.title)
+          end
+
+          it "flashes a success message" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: course.description } 
+
+            expect(flash[:success]).to be_present
+          end
+          it "redirects to the index page" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: course.description } 
+
+            expect(response).to redirect_to admin_courses_path 
+          end
+       end
+       context "with invalid attributes " do
+          login_admin
+          it "doesn't update an existing course" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: nil} 
+            course.reload
+            expect(Course.find(course.id).description).not_to eq("") 
+          end
+          it "renders the edit page" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: nil }
+            expect(response).to render_template :edit 
+          end
+          it "flashes an error message" do
+            put :update, id: course.id, course: { id: course.id, title: "title new", description: nil } 
+
+            expect(flash[:alert]).to be_present
+          end
+       end
+    end
+    describe "DELETE #destroy" do
+        let(:course) { FactoryGirl.create(:course) } 
+        it_behaves_like "sign in mandatory" do
+            let(:action) { delete :destroy, id: course.id }
+        end
+        it_behaves_like "admin mandatory" do
+            let(:action) { delete :destroy, id: course.id }
+        end
+        context "delete a course" do
+            login_admin
+            it "redirects to the index page" do
+              delete :destroy, id: course.id
+              expect(response).to redirect_to admin_courses_path
+            end
+            it "deletes a course" do
+              delete :destroy, id: course.id
+              expect(Course.count).to eq(0)
+            end
+            it "flashes a success message" do
+              delete :destroy, id: course.id
+              expect(flash[:success]).to be_present 
+            end
         end
     end
 
